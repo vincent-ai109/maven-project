37f7959c05257f31631c3de6f351f708aad64239
vincent-ai109 <80821478+vincent-ai109@users.noreply.github.com>
2021-07-02 15:31:57 +0200

    Update Greeter.java
    commentaire

--- server/src/main/java/com/example/Greeter.java
+++ server/src/main/java/com/example/Greeter.java
@@ -1,13 +1,22 @@
 package com.example;
 
-
+ /**
+  * This is a class.
+  */
+  
 public class Greeter {
 
-  
+ /**
+  * This is a constructeur
+  */
   public Greeter() {
 
   }
-
+/**
+  * @param someone name of a person
+  * @return greeting string
+  */
+  
   public final String greet(final String someone) {
     return String.format("Hello, %s!", someone);
   }
